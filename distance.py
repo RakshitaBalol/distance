c96ef47627ee8f45ead2c61929c274c34c739433
RakshitaBalol <rakshitabalol18@gmail.com>
2025-12-05 21:38:04 +0530

    Add files via upload

---
+++ distance.py
@@ -0,0 +1,9 @@
+# Input: speed and time
+speed = float(input("Enter the speed of the vehicle: "))
+time = float(input("Enter the time traveled: "))
+
+# Formula: Distance = Speed × Time
+distance = speed * time
+
+# Output
+print("Distance traveled =", distance)
